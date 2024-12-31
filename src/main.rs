f284a8d9ca636157134442b199fd2cd207928d34
YatFungLoo <looyatfung@gmail.com>
2024-12-31 11:28:23 +0800

    added insertion sort and simple test.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-fn print_array(array: &[i8]) {
+pub fn print_array(array: &[i8]) {
     // for element in array {
     let length = array.len();
     for (index, element) in array.iter().enumerate() {
@@ -10,21 +10,29 @@ fn print_array(array: &[i8]) {
     }
 }
 
-fn selection_sort(array: &mut [i8]) {
+pub fn selection_sort(array: &mut [i8]) {
     let length = array.len();
     for i in 0..length {
-	for j in i+1..length {
-	    if array[j] < array[i] {
-		array.swap(i, j);
-	    }
-	}
+        for j in i + 1..length {
+            if array[j] < array[i] {
+                array.swap(i, j);
+            }
+        }
+    }
+}
+
+pub fn insertion_sort(array: &mut [i8]) {
+    let length = array.len();
+    for i in 1..=length {
+        for j in (1..i).rev() {
+            if array[j] < array[j - 1] {
+                array.swap(j, j - 1);
+            }
+        }
     }
 }
 
 fn main() {
-    let mut int_array: [i8; 10] = [1, 6, 7, 3, 2, 4, 9, 8, 0, 5];
-    selection_sort(&mut int_array);
-    print_array(&int_array);
 }
 
 #[cfg(test)]
@@ -32,10 +40,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-	let mut int_array: [i8; 10] = [1, 6, 7, 3, 2, 4, 9, 8, 0, 5];
-	let answer: [i8; 10] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    fn selection_sort_test() {
+        let mut int_array: [i8; 10] = [1, 6, 7, 3, 2, 4, 9, 8, 0, 5];
+        let answer: [i8; 10] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
         selection_sort(&mut int_array);
+        assert_eq!(int_array, answer);
+    }
+
+    #[test]
+    fn insertion_sort_test() {
+        let mut int_array: [i8; 10] = [1, 6, 7, 3, 2, 4, 9, 8, 0, 5];
+        let answer: [i8; 10] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        insertion_sort(&mut int_array);
         assert_eq!(int_array, answer);
     }
 }
